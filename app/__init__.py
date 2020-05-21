7977cb38fa75c6f1c3a1be20ed3e7c3dfe15a689
Aydar <dusmukhambetov@mail.ru>
2020-05-21 15:37:59 +0300

    init mongo_uri for test

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,9 @@ from app.blueprints.user import bp as user_bp
 from app.db.mongo import mongo
 
 app = Flask(__name__, instance_relative_config=True)
+mongo_uri = os.getenv('DB') if os.getenv('DB') else 'mongodb://localhost:27017/banana'
 app.config.from_mapping(
-        MONGO_URI=os.getenv('DB'),
+        MONGO_URI=mongo_uri,
         SECRET_KEY=os.getenv('SECRET_KEY'),
 )
 
