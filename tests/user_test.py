d057406bdd9a7b0d1c6e44b7f8c6441befd40026
Aydar <dusmukhambetov@mail.ru>
2020-06-09 09:20:22 +0300

    access_token changed

--- tests/user_test.py
+++ tests/user_test.py
@@ -6,7 +6,9 @@ from tests.setup import TestSetup
 TEST_PASSWORD = 'test_pass'
 TEST_USERNAME = 'test_user'
 
+
 class UserTestCase(TestSetup):
+
     def test_get_user_data(self):
         user_id = str(app_module.mongo.db.users.insert_one({
             'username': TEST_USERNAME,
@@ -23,7 +25,7 @@ class UserTestCase(TestSetup):
         res = self.app.post(
             '/get_user_data',
             json={
-                'accessToken': encoded_user_id.decode('utf-8')
+                'access_token': encoded_user_id.decode('utf-8')
             }
         )
         res = res.get_json()
