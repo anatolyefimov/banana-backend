dc2bb0fd4dab83163a70ed273089cfccbf897f3d
Aydar <dusmukhambetov@mail.ru>
2020-06-09 09:14:35 +0300

    format for pep8. иногда не очень красиво, в одну строчку скорее проще. но там в 100 знаков ограничение

--- tests/basket_test.py
+++ tests/basket_test.py
@@ -4,6 +4,7 @@ from tests.setup import TestSetup
 
 TEST_PASSWORD = 'test_pass'
 TEST_USERNAME = 'test_user'
+credentials_json = {'username': TEST_USERNAME, 'password': TEST_PASSWORD}
 
 product1 = new_product(name='test1', price=1)
 product2 = new_product(name='test2', price=2)
@@ -13,7 +14,9 @@ class BasketTestCase(TestSetup):
 
     def test_add_product_to_basket(self):
         access_token, product1_id, product2_id = self._auth_preparation()
-        res2 = self.app.post('/add_to_basket', json={'product_id': product2_id, 'access_token': access_token}).get_json()
+        res2 = self.app.post(
+            '/add_to_basket',
+            json={'product_id': product2_id, 'access_token': access_token}).get_json()
 
         basket = res2['basket']
         total_sum = res2['total_sum']
@@ -25,7 +28,9 @@ class BasketTestCase(TestSetup):
 
     def test_delete_product_from_basket(self):
         access_token, product1_id, product2_id = self._auth_preparation()
-        res2 = self.app.post('/remove_from_basket', json={'product_id': product2_id, 'access_token': access_token}).get_json()
+        res2 = self.app.post(
+            '/remove_from_basket',
+            json={'product_id': product2_id, 'access_token': access_token}).get_json()
         basket = res2['basket']
         total_sum = res2['total_sum']
 
@@ -33,7 +38,9 @@ class BasketTestCase(TestSetup):
         assert product2_id not in basket
         assert total_sum == 1
 
-        res2 = self.app.post('/remove_from_basket', json={'product_id': product1_id, 'access_token': access_token}).get_json()
+        res2 = self.app.post(
+            '/remove_from_basket',
+            json={'product_id': product1_id, 'access_token': access_token}).get_json()
         basket = res2['basket']
         total_sum = res2['total_sum']
 
@@ -41,15 +48,17 @@ class BasketTestCase(TestSetup):
         assert total_sum == 0
 
     def _auth_preparation(self):
-        self.app.post('/register', json={'username': TEST_USERNAME, 'password': TEST_PASSWORD})
-        res = self.app.post('/login', json={'username': TEST_USERNAME, 'password': TEST_PASSWORD})
+        self.app.post('/register', json=credentials_json)
+        res = self.app.post('/login', json=credentials_json)
         res_data = res.get_json()
         access_token = res_data['access_token']
 
         product1_id = str(app_module.mongo.db.catalog.insert_one(product1).inserted_id)
         product2_id = str(app_module.mongo.db.catalog.insert_one(product2).inserted_id)
 
-        self.app.post('/add_to_basket', json={'product_id': product1_id, 'access_token': access_token})
-        self.app.post('/add_to_basket', json={'product_id': product2_id, 'access_token': access_token})
+        self.app.post('/add_to_basket',
+                      json={'product_id': product1_id, 'access_token': access_token})
+        self.app.post('/add_to_basket',
+                      json={'product_id': product2_id, 'access_token': access_token})
 
         return access_token, product1_id, product2_id
